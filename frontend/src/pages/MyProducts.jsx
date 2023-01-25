2da77ea21c175d6456c40410c7f29cf6025cb4cc
maaghia <toubalmaria23@gmail.com>
2023-01-25 23:59:37 +0100

    managed to get user id but didnt filter products

--- frontend/src/pages/MyProducts.jsx
+++ frontend/src/pages/MyProducts.jsx
@@ -1,41 +1,26 @@
 import React, { useEffect, useState, useContext } from "react";
-import useFetchData from "../hooks/useFetchData";
-import Product from "../components/Product";
 import { Auth } from "../contexts/Auth";
-
-
 export default function MyProducts() {
     const { user } = useContext(Auth);
+    console.log("from my products",user.id)
+    const [products, setProducts] = useState([]);
     
-  // const {isLoading, apiData, serverError } = useFetchData("/api/products");
-  // console.log(apiData);
+   useEffect(() => {
+    const fetchProducts = async (userid) => {
+        const response = await fetch(`http://localhost:3000/api/products/postedBy/${userid}`)
+        .then((response)=> response.json())
+        .then((data)=>setProducts(data))
+    }
+    fetchProducts(user.id);
+    },[]); 
 
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/products")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        return response.json();
-        
-      })
-      .then((data) => {
-        setProducts(data);
-      })
-      .catch((e) => console.log(e));
-  }, []);
-  
   return (
     <div>
-      <div className="flex flex-wrap gap-10 justify-center">
+        {/* <div className="flex flex-wrap gap-10 justify-center">
         { products?.filter(product => {
-          return <Product product={product} key={product._id}/>;
+          return <Product{product.postedBy}==={user.id}/>;
         })}
-      </div>
+      </div> */}  
     </div>
   );
 }
