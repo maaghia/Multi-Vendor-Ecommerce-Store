2cb15a38eb7b3067b3568a89e93262892f1f1bd9
maaghia <toubalmaria23@gmail.com>
2023-02-25 11:46:34 +0100

    added search by category to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useContext, useState, useEffect} from "react";
 import { Auth } from "../contexts/Auth";
 import { useLogout } from "../hooks/useLogout";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar(){
     const { user } = useContext(Auth);
@@ -23,10 +24,23 @@ export default function Navbar(){
         const htmlTag = document.getElementById('htmlTag');
         htmlTag.setAttribute('data-theme', theme);
       }, [theme]);
-  
+      
+      //search by category 
+      const [category, setCategory] = useState("");
+    
+        const navigate = useNavigate();
+
+        const handleSearch = () => {
+        console.log(category, location);
+
+        if (category) {
+            navigate(`/byCategory/${category}`);
+        }
+        };
+
     
     return(
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 mb-10">
             <div className="">
                     <label className="swap swap-rotate">
                     {/* this hidden checkbox controls the state */} 
@@ -41,18 +55,45 @@ export default function Navbar(){
                     {/* moon icon */}
                     <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
                     </label>
-                </div>
+            </div>
             <div className="flex-1">
                 <a className="btn btn-ghost normal-case text-xl">BAZAR</a>
             </div>
-            <div className="flex-none">
-                <NavLink to="/search" tabIndex={0} className="btn btn-ghost btn-circle">
-                    <div className="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                    </svg> 
+            <div className="flex">
+                <div className="flex flex-row space-y-1">
+                    <div className="form-control w-full max-w-xs mb-1">
+                    <label className="label">
+                    </label>
+                    <select 
+                            value={category} 
+                            onChange={(e) => {
+                            setCategory(e.target.value);
+                            }}
+                            className="select select-ghost w-full max-w-xs mb-1">
+                        <option value="">Category</option>
+                        <option value="Clothes">Clothes</option>
+                        <option value="Dishes">Dishes</option>
+                        <option value="Toys">Toys</option>
+                        <option value="Electronics">Electronics</option>
+                        <option value="Cosmetics">Cosmetics</option>
+                        <option value="Accesories">Accesories</option>
+                        <option value="Home Furniture">Home Furniture</option>
+                        <option value="Sport Materials">Sport Materials</option>
+                        <option value="Sea Materials">Sea Materials</option>
+                    </select>
                     </div>
-                </NavLink>
+                    
+                    <div>
+                    <button onClick={handleSearch} className="btn btn-ghost btn-circle mt-3">
+                        <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg> 
+                        </div>
+                    </button>
+                    </div>
+                </div>
+                
                 
 
                 {user && (
@@ -78,6 +119,6 @@ export default function Navbar(){
                 )}
                 
             </div>
-            </div>
+        </div>
     );
 }
