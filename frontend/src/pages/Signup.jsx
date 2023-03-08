175cac8ade16417f31fe4e2e392ca847be3d4846
maaghia <toubalmaria23@gmail.com>
2023-03-09 00:09:24 +0100

    signup rendering

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { useSignup } from "../hooks/useSignup";
 
-export default function Login(){
+export default function Signup(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [phoneNbr, setPhoneNbr] = useState("");
